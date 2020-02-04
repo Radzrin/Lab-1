e6eccc94eb609332c218e81ff37bab2faf724ae6
Radzrin <60394053+Radzrin@users.noreply.github.com>
2020-02-04 10:13:58 -0500

    Add files via upload

---
+++ MyAgeIs.java
@@ -0,0 +1,18 @@
+public class MyAgeIs {
+
+	public static void main(String[] args) {
+		System.out.println("\nAgeTest");
+
+		int (currentYear);
+		int (yearOfBirth);
+		final int factor = 2002
+
+		currentYear = 2020;
+		yearOfBirth = 2002
+		age = currentYear - yearOfBirth;
+
+		System.out.println("\n" + currentYear - yearOfBirth "age.");
+		
+	}
+
+}
