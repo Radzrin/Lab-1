d020997f79262a3674af8a5a514f0fc0f8c8a567
Radzrin <60394053+Radzrin@users.noreply.github.com>
2020-02-04 09:58:39 -0500

    Add files via upload

---
+++ meters.java
@@ -0,0 +1,18 @@
+public class converter {
+
+	 public static void main(String[] args) {
+	 	System.out.println("\n1 meter is equal to 3.28 feet.")
+
+	 	double distanceInMeters;
+	 	double distanceInFeet;
+	 	final double factor = 3.28084
+	 	
+	 	distanceInMeters = 4;
+	 	distanceInFeet = factor * distanceInMeters;
+
+	 	System.out.println("\n" + distanceInMeters + distanceInFeet + "feet." )
+
+	 }
+
+}
+
