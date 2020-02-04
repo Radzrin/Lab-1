31e05b1e93ffa4ee0e07a3057e62b5ace864d5d3
Radzrin <60394053+Radzrin@users.noreply.github.com>
2020-02-04 10:28:14 -0500

    Add files via upload

---
+++ currency.java
@@ -0,0 +1,15 @@
+public class currency {
+
+	public static void main(string[] args)	{
+
+		double CAD 
+		double USD 
+		double EUR 
+
+		double CAD = 0.75 USD
+		double USD = 0.91 EUR
+		double EUR = 0.68 CAD
+
+	}
+
+}
