235189c19704b8fd625e61ab699a595d8268c5dc
Radzrin <60394053+Radzrin@users.noreply.github.com>
2020-02-04 10:26:46 -0500

    Add files via upload

---
+++ rectangle.java
@@ -0,0 +1,22 @@
+public class converter {
+
+	 public static void main(String[] args) {
+
+	 	System.out.println("rectangles");
+
+	 	double length;
+	 	double width;
+	 	double perimeter;
+	 	double area;
+	 	length 12;
+	 	width 5;
+
+	 	perimeter = (2 * length + 2 * width);
+	 	area = (lenght * width);
+
+	 	System.out.println("\n" + area "area")
+	 	System.out.println("\n" + perimeter "perimeter" )
+
+	 }	
+
+}
